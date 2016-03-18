2b0380e65449fa2a3336c96c886ec4645b80eaac
Noorani Bakerally <noorani.bakerally@gmail.com>
2016-03-18 19:44:12 +0100

    CMD generator added

--- src/main/java/com/github/thesmartenergy/sparql/generate/generator/CMDGenerator.java
+++ src/main/java/com/github/thesmartenergy/sparql/generate/generator/CMDGenerator.java
@@ -17,8 +17,37 @@ package com.github.thesmartenergy.sparql.generate.generator;
 
 /**
  *
- * @author bakerally
+ * @author Noorani Bakerally
  */
+import com.github.thesmartenergy.sparql.generate.jena.SPARQLGenerate;
+import com.github.thesmartenergy.sparql.generate.jena.SPARQLGenerateException;
+import com.github.thesmartenergy.sparql.generate.jena.query.SPARQLGenerateQuery;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.StringWriter;
+import java.net.URI;
+import java.net.URL;
+import org.apache.commons.io.IOUtils;
+import org.apache.jena.query.QueryFactory;
+import org.apache.jena.query.QuerySolutionMap;
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.riot.RDFDataMgr;
+import org.apache.jena.util.FileManager;
+import org.apache.jena.util.LocationMapper;
+import org.apache.jena.util.Locator;
+import org.apache.jena.util.LocatorFile;
+import org.apache.log4j.Logger;
 public class CMDGenerator {
-    
+    public static void main(String [] args){
+        System.out.println("Hellos");
+        if( args.length > 0 ) {
+          for (String arg:args){
+              System.out.println(arg);
+          }
+        }
+    }
 }
