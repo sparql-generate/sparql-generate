905be43bac582ec7212a387dde96e2b7c01ca03d
Noorani Bakerally <noorani.bakerally@gmail.com>
2016-03-18 16:22:27 +0100

    Files corrected

--- src/main/java/com/github/thesmartenergy/sparql/generate/jena/function/library/FN_HTMLTag.java
+++ src/main/java/com/github/thesmartenergy/sparql/generate/jena/function/library/FN_HTMLTag.java
@@ -16,44 +16,19 @@
 package com.github.thesmartenergy.sparql.generate.jena.function.library;
 
 import com.github.thesmartenergy.sparql.generate.jena.SPARQLGenerate;
-import java.io.ByteArrayInputStream;
-import java.io.InputStream;
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.xpath.XPath;
-import javax.xml.xpath.XPathConstants;
-import javax.xml.xpath.XPathFactory;
-import org.apache.jena.graph.Node;
-import java.math.BigDecimal;
-import static org.apache.jena.query.ResultSetFactory.result;
 import org.apache.jena.sparql.expr.ExprEvalException;
 import org.apache.jena.sparql.expr.NodeValue;
 import org.apache.jena.sparql.expr.nodevalue.NodeValueString;
 import org.apache.jena.sparql.function.FunctionBase2;
 import org.apache.log4j.Logger;
-import org.w3c.dom.Document;
-import org.apache.jena.sparql.expr.nodevalue.NodeValueBoolean;
-import org.apache.jena.sparql.expr.nodevalue.NodeValueDecimal;
-import org.apache.jena.sparql.expr.nodevalue.NodeValueDouble;
-import org.apache.jena.sparql.expr.nodevalue.NodeValueFloat;
-import org.apache.jena.sparql.expr.nodevalue.NodeValueInteger;
-import org.apache.jena.sparql.expr.nodevalue.NodeValueString;
+import org.jsoup.Jsoup;
+import org.jsoup.select.Elements;
+
 /**
- * A SPARQL Function that extracts a string from a XML document, according to a
- * XPath expression. The Function URI is
- * {@code <http://w3id.org/sparql-generate/fn/XPath>}.
- * It takes two parameters as input:
- * <ul>
- * <li>a RDF Literal with datatype URI
- * {@code <urn:iana:mime:application/json>}</li>
- * <li>a RDF Literal with datatype {@code xsd:string}</li>
- * </ul>
- * and returns a RDF Literal with datatype URI
- * {@code <urn:iana:mime:application/json>}.
  *
- * @author maxime.lefrancois
+ * @author Noorani Bakerally
  */
-public class FN_HTMLTag extends FunctionBase2 {
+public class FN_HTMLTag extends FunctionBase2{
     //TODO write multiple unit tests for this class.
 
     /**
@@ -79,19 +54,24 @@ public class FN_HTMLTag extends FunctionBase2 {
      * @return -
      */
     @Override
-    public NodeValue exec(NodeValue xml, NodeValue xpath) {
-        if (xml.getDatatypeURI() == null
+    public NodeValue exec(NodeValue html, NodeValue v2) {
+        if (html.getDatatypeURI() == null
                 && datatypeUri == null
-                || xml.getDatatypeURI() != null
-                && !xml.getDatatypeURI().equals(datatypeUri)
-                && !xml.getDatatypeURI().equals("http://www.w3.org/2001/XMLSchema#string")) {
+                || html.getDatatypeURI() != null
+                && !html.getDatatypeURI().equals(datatypeUri)
+                && !html.getDatatypeURI().equals("http://www.w3.org/2001/XMLSchema#string")) {
             LOG.warn("The URI of NodeValue1 MUST be <" + datatypeUri + ">"
                     + "or <http://www.w3.org/2001/XMLSchema#string>."
                     + " Returning null.");
         }
       
         try {
-            return new NodeValueString("HTML Tag");
+            String sourceHtml = String.valueOf(html.asNode().getLiteralValue());
+            org.jsoup.nodes.Document htmldoc = Jsoup.parse(sourceHtml);
+            
+            String selectPath = String.valueOf(v2.asNode().getLiteralValue());
+            Elements elements = htmldoc.select(selectPath);
+            return new NodeValueString(elements.text());
         } catch (Exception e) {
             LOG.debug("Error:HTML Tag "+e.getMessage());
             throw new ExprEvalException("FunctionBase: no evaluation", e);
