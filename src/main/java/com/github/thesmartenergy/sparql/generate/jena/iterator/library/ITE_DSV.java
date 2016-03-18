a45a11e26dcdcabaa4c85853f9baccdc359fd63c
Noorani Bakerally <noorani.bakerally@gmail.com>
2016-03-18 06:53:02 +0100

    Iterator DSV added

--- src/main/java/com/github/thesmartenergy/sparql/generate/jena/iterator/library/ITE_DSV.java
+++ src/main/java/com/github/thesmartenergy/sparql/generate/jena/iterator/library/ITE_DSV.java
@@ -16,19 +16,59 @@
 package com.github.thesmartenergy.sparql.generate.jena.iterator.library;
 
 import com.github.thesmartenergy.sparql.generate.jena.SPARQLGenerate;
-import com.github.thesmartenergy.sparql.generate.jena.iterator.IteratorFunctionBase2;
+import java.io.ByteArrayInputStream;
 import java.util.ArrayList;
+import com.github.thesmartenergy.sparql.generate.jena.iterator.IteratorFunctionBase2;
+import java.io.StringWriter;
 import java.util.List;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathFactory;
+import org.apache.jena.datatypes.RDFDatatype;
+import org.apache.jena.datatypes.TypeMapper;
+import org.apache.jena.graph.Node;
+import org.apache.jena.graph.NodeFactory;
+import org.apache.jena.sparql.expr.ExprEvalException;
 import org.apache.jena.sparql.expr.NodeValue;
-import org.apache.jena.sparql.expr.nodevalue.NodeValueString;
+import org.apache.jena.sparql.expr.nodevalue.NodeValueNode;
 import org.apache.log4j.Logger;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.apache.jena.sparql.expr.nodevalue.NodeValueBoolean;
+import org.apache.jena.sparql.expr.nodevalue.NodeValueDecimal;
+import org.apache.jena.sparql.expr.nodevalue.NodeValueDouble;
+import org.apache.jena.sparql.expr.nodevalue.NodeValueFloat;
+import org.apache.jena.sparql.expr.nodevalue.NodeValueInteger;
+import org.apache.jena.sparql.expr.nodevalue.NodeValueNode;
+import org.apache.jena.sparql.expr.nodevalue.NodeValueString;
+import java.math.BigDecimal;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.dom.DOMSource; 
+import javax.xml.transform.stream.StreamResult;
 
 /**
+ * A SPARQL Iterator function that extracts a list of sub-XML elements of a
+ * XML root element, according to a XPath expression. The Iterator function URI is
+ * {@code <http://w3id.org/sparql-generate/ite/XPath>}.
+ * It takes two parameters as input:
+ * <ul>
+ * <li>a RDF Literal with datatype URI
+ * {@code <urn:iana:mime:application/xml>}</li>
+ * <li>a RDF Literal with datatype {@code xsd:string}</li>
+ * </ul>
+ * and returns a list of RDF Literal with datatype URI
+ * {@code <urn:iana:mime:application/xml>}.
  *
- * @author bakerally
+ * @author maxime.lefrancois
  */
 public class ITE_DSV extends IteratorFunctionBase2 {
-     /**
+
+    /**
      * The logger.
      */
     private static final Logger LOG = Logger.getLogger(ITE_DSV.class);
@@ -42,15 +82,31 @@ public class ITE_DSV extends IteratorFunctionBase2 {
      * The datatype URI of the first parameter and the return literals.
      */
     private static final String datatypeUri = "urn:iana:mime:application/xml";
-    
+
     @Override
-    public List<NodeValue> exec(NodeValue v1, NodeValue v2) {
+    public List<NodeValue> exec(NodeValue xml, NodeValue v2) {
         
-        List<NodeValue> nodeValues = new ArrayList<>(2);
-        nodeValues.add(new NodeValueString("1"));
-        nodeValues.add(new NodeValueString("2"));
-        
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        /*
+        if (xml.getDatatypeURI() == null
+                && datatypeUri == null
+                || xml.getDatatypeURI() != null
+                && !xml.getDatatypeURI().equals(datatypeUri)
+                && !xml.getDatatypeURI().equals("http://www.w3.org/2001/XMLSchema#string")) {
+            LOG.warn("The URI of NodeValue1 MUST be"
+                    + " <" + datatypeUri + "> or"
+                    + " <http://www.w3.org/2001/XMLSchema#string>. Got <"
+                    + xml.getDatatypeURI() + ">. Returning null.");
+        }
+        */
+        try {
+            
+            List<NodeValue> nodeValues = new ArrayList<>(2);
+            nodeValues.add(new NodeValueString("1"));
+            nodeValues.add(new NodeValueString("2"));
+           
+            return nodeValues;  
+        } catch (Exception e) {
+            throw new ExprEvalException("FunctionBase: no evaluation", e);
+        }
     }
-    
 }
