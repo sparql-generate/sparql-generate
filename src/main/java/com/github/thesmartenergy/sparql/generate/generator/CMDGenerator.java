c546823b2209c7e579d17ef5a5be7157b83561c4
Noorani Bakerally <noorani.bakerally@gmail.com>
2016-03-19 01:59:37 +0100

    adding option for disabling logging

--- src/main/java/com/github/thesmartenergy/sparql/generate/generator/CMDGenerator.java
+++ src/main/java/com/github/thesmartenergy/sparql/generate/generator/CMDGenerator.java
@@ -21,9 +21,13 @@ package com.github.thesmartenergy.sparql.generate.generator;
  */
 import com.github.thesmartenergy.sparql.generate.jena.SPARQLGenerate;
 import com.github.thesmartenergy.sparql.generate.jena.SPARQLGenerateException;
+import com.github.thesmartenergy.sparql.generate.jena.engine.PlanFactory;
+import com.github.thesmartenergy.sparql.generate.jena.engine.RootPlan;
 import com.github.thesmartenergy.sparql.generate.jena.lang.ParseException;
 import com.github.thesmartenergy.sparql.generate.jena.query.SPARQLGenerateQuery;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -31,6 +35,8 @@ import java.io.OutputStream;
 import java.io.StringWriter;
 import java.net.URI;
 import java.net.URL;
+import java.util.Arrays;
+import java.util.List;
 import java.util.logging.Level;
 import org.apache.commons.cli.BasicParser;
 import org.apache.commons.cli.CommandLine;
@@ -47,20 +53,26 @@ import org.apache.jena.util.Locator;
 import org.apache.jena.util.LocatorFile;
 import org.apache.log4j.Logger;
 import org.apache.commons.cli.Options;
+import org.apache.jena.atlas.logging.Log;
 import org.apache.log4j.Logger;
 
 public class CMDGenerator {
     
     static Logger LOG;
-     
+    static FileManager fileManager;
+    
     public static void main(String [] args){
         
-        LOG = Logger.getLogger(CMDGenerator.class);
+        
+       
+        List<String> formats = Arrays.asList("TTL","TURTLE","NTRIPLES","TRIG","RDFXML","JSONLD");
+        
         //command line options goes here
         Options opt = new Options()
-                .addOption("qf", "queryfile", true, "Path to the file containing the SPARGL query")
-                .addOption("h", false, "Print help")
-                
+                .addOption("h", false, "Show help")
+                .addOption("qf", "queryfile", true, "Local path to the file containing the SPARGL query")
+                .addOption("f","outputformat",true,"Output RDF format, e.g. -f TTL. Possible serializations are: TTL for Turtle, NTRIPLES for NTRIPLES, RDFXML for RDF/XML, N3 for N3, JSONLD for JSON-LD, TRIG for TRIG")
+                .addOption("l", false, "Disable logging, by default logging is enabled")
                 ;
                 
         try {
@@ -68,18 +80,73 @@ public class CMDGenerator {
             BasicParser parser = new BasicParser();
             CommandLine cl = parser.parse(opt, args);
             
+            
+            String query = "";
+            String outputFormat = "TTL";
+            
             //print help menu
             if ( cl.hasOption('h') ) {
                 HelpFormatter f = new HelpFormatter();
                 f.printHelp("OptionsTip", opt);
+                return;
             } 
             
+            LOG = Logger.getLogger(CMDGenerator.class);
+            fileManager = FileManager.makeGlobal(); 
+            if ( cl.hasOption('l') ) {
+                LOG.setLevel(org.apache.log4j.Level.OFF);
+            }
+            
+            
+            
+        
             //get query file path
             if (cl.hasOption("qf")){
-                System.out.println(cl.getOptionValue("qf"));
+                String file_path = cl.getOptionValue("qf");
+                File f = new File(file_path);
+                if(f.exists() && !f.isDirectory()) { 
+                    FileInputStream fisTargetFile = new FileInputStream(f);
+                    query = IOUtils.toString(fisTargetFile, "UTF-8");    
+                    LOG.debug("\n\nRead SPARGL Query ..\n"+query+"\n\n");
+                } else {
+                    LOG.error("File "+file_path+" not found.");
+                }   
             }
-       
+            if (cl.hasOption("f")){
+                String format = cl.getOptionValue("f");
+                if (formats.contains(format)){
+                    outputFormat = format;
+                } else {
+                    LOG.error("Invalid output format,"+cl.getOptionProperties("f").getProperty("description"));
+                    return;
+                }
+                
+            }
+         
+            LOG.debug("Processing Query");
+            SPARQLGenerateQuery q = (SPARQLGenerateQuery) QueryFactory.create(query, SPARQLGenerate.SYNTAX);
+            
+            PlanFactory factory = new PlanFactory(fileManager);
+            RootPlan plan = factory.create(q);
+            Model output = ModelFactory.createDefaultModel();
+            QuerySolutionMap initialBinding = null;
+
+            // execute plan
+            plan.exec(initialBinding, output);
+            
+            StringWriter sw = new StringWriter();
+            
+            
+            output.write(sw,outputFormat);
+            
+            System.out.println(sw.toString());
+            
+            
         } catch (org.apache.commons.cli.ParseException ex) {
+            LOG.error(ex);
+        } catch (FileNotFoundException ex) {
+            LOG.error(ex);
+        } catch (IOException ex) {
             LOG.error(ex);
         }
         
