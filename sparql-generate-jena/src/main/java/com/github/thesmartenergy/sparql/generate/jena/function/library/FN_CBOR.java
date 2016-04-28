ab473f1e82259a0f258dd11763480d379e48fa08
Noorani Bakerally <noorani.bakerally@gmail.com>
2016-04-28 14:42:32 +0200

    cbor example 1 added

---
+++ sparql-generate-jena/src/main/java/com/github/thesmartenergy/sparql/generate/jena/function/library/FN_CBOR.java
@@ -0,0 +1,133 @@
+/*
+ * Copyright 2016 ITEA 12004 SEAS Project.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.thesmartenergy.sparql.generate.jena.function.library;
+
+import com.github.thesmartenergy.sparql.generate.jena.SPARQLGenerate;
+import com.jayway.jsonpath.JsonPath;
+import java.math.BigDecimal;
+import net.minidev.json.JSONArray;
+import org.apache.jena.sparql.expr.ExprEvalException;
+import org.apache.jena.sparql.expr.NodeValue;
+import org.apache.jena.sparql.expr.nodevalue.NodeValueBoolean;
+import org.apache.jena.sparql.expr.nodevalue.NodeValueDecimal;
+import org.apache.jena.sparql.expr.nodevalue.NodeValueDouble;
+import org.apache.jena.sparql.expr.nodevalue.NodeValueFloat;
+import org.apache.jena.sparql.expr.nodevalue.NodeValueInteger;
+import org.apache.jena.sparql.expr.nodevalue.NodeValueString;
+import org.apache.jena.sparql.function.FunctionBase2;
+import org.apache.log4j.Logger;
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+import com.sun.xml.internal.messaging.saaj.util.Base64;
+import org.apache.jena.graph.Node;
+import org.apache.jena.graph.NodeFactory;
+import org.apache.jena.sparql.expr.nodevalue.NodeValueNode;
+
+/**
+ * A SPARQL Function that extracts a string from a JSON document, according to a
+ * JSONPath expression. The Function URI is
+ * {@code <http://w3id.org/sparql-generate/fn/JSON_Path_jayway>}.
+ * It takes two parameters as input:
+ * <ul>
+ * <li>a RDF Literal with datatype URI
+ * {@code <urn:iana:mime:application/json>}</li>
+ * <li>a RDF Literal with datatype {@code xsd:string}</li>
+ * </ul>
+ * and returns a RDF Literal with datatype URI
+ * {@code <urn:iana:mime:application/json>}.
+ *
+ * @author maxime.lefrancois
+ */
+public final class FN_CBOR extends FunctionBase2 {
+    //TODO write multiple unit tests for this class.
+
+    /**
+     * The logger.
+     */
+    private static final Logger LOG = Logger.getLogger(FN_CBOR.class);
+
+    /**
+     * The SPARQL function URI.
+     */
+    public static final String URI = SPARQLGenerate.FN + "CBOR";
+
+    /**
+     * The datatype URI of the first parameter and the return literals.
+     */
+    private static final String datatypeUri = "urn:iana:mime:application/json";
+
+    /**
+     * Returns the evaluation of JSONPath {@code jsonpath} over the JSON
+     * document {@code json}.
+     * @param json the RDF Literal that represents a JSON document
+     * @param jsonpath the xsd:string that represents the JSONPath
+     * @return
+     */
+    @Override
+    public NodeValue exec(NodeValue cbor, NodeValue jsonpath) {
+        /*
+        if (json.getDatatypeURI() == null
+                && datatypeUri == null
+                || json.getDatatypeURI() != null
+                && !json.getDatatypeURI().equals(datatypeUri)
+                && !json.getDatatypeURI().equals("http://www.w3.org/2001/XMLSchema#string")) {
+            LOG.warn("The URI of NodeValue1 MUST be <" + datatypeUri + ">"
+                    + "or <http://www.w3.org/2001/XMLSchema#string>."
+                    + " Returning null.");
+        } */
+
+        String json = Base64.base64Decode(cbor.asNode().getLiteralLexicalForm());
+        try {
+            Object value = JsonPath.parse(json)
+                    .limit(1).read(jsonpath.getString());
+            
+            if (value instanceof String) {
+                return new NodeValueString((String) value);
+            } else if (value instanceof Float) {
+                return new NodeValueFloat((Float) value);
+            } else if (value instanceof Boolean) {
+                return new NodeValueBoolean((Boolean) value);
+            } else if (value instanceof Integer) {
+                return new NodeValueInteger((Integer) value);
+            } else if (value instanceof Double) {
+                return new NodeValueDouble((Double) value);
+            } else if (value instanceof BigDecimal) {
+                return new NodeValueDecimal((BigDecimal) value);
+            } else {
+                String strValue = String.valueOf(value);
+                
+                
+                JsonParser parser = new JsonParser();
+                JsonElement valElement = parser.parse(strValue);
+                JsonArray list = valElement.getAsJsonArray();
+                
+                if (list.size() == 1){
+                    String jsonstring = list.get(0).getAsString();
+                    Node node = NodeFactory.createLiteral(jsonstring);
+                    NodeValue nodeValue = new NodeValueNode(node);
+                    return nodeValue;
+                    
+                } else {
+                  return new NodeValueString(String.valueOf(value)); 
+                }
+            }
+        } catch (Exception e) {
+            throw new ExprEvalException("FunctionBase: no evaluation", e);
+        }
+    }
+}
---
+++ sparql-generate-jena/src/main/java/com/github/thesmartenergy/sparql/generate/jena/iterator/library/ITE_CBOR.java
@@ -0,0 +1,141 @@
+/*
+ * Copyright 2016 ITEA 12004 SEAS Project.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.thesmartenergy.sparql.generate.jena.iterator.library;
+
+
+import com.github.thesmartenergy.sparql.generate.jena.SPARQLGenerate;
+import com.google.gson.Gson;
+import com.jayway.jsonpath.Configuration;
+import com.jayway.jsonpath.JsonPath;
+import com.jayway.jsonpath.Option;
+import com.jayway.jsonpath.spi.json.JacksonJsonProvider;
+import com.jayway.jsonpath.spi.json.JsonProvider;
+import com.jayway.jsonpath.spi.mapper.JacksonMappingProvider;
+import com.jayway.jsonpath.spi.mapper.MappingProvider;
+import com.github.thesmartenergy.sparql.generate.jena.iterator.IteratorFunctionBase2;
+import com.sun.xml.internal.messaging.saaj.util.Base64;
+import java.util.ArrayList;
+import java.util.EnumSet;
+import java.util.List;
+import java.util.Set;
+import org.apache.jena.datatypes.RDFDatatype;
+import org.apache.jena.datatypes.TypeMapper;
+import org.apache.jena.graph.Node;
+import org.apache.jena.graph.NodeFactory;
+import org.apache.jena.sparql.expr.ExprEvalException;
+import org.apache.jena.sparql.expr.NodeValue;
+import org.apache.jena.sparql.expr.nodevalue.NodeValueNode;
+import org.apache.log4j.Logger;
+
+/**
+ * A SPARQL Iterator function that extracts a list of sub-JSON documents of a
+ * JSON document, according to a JSONPath expression. The Iterator function URI is
+ * {@code <http://w3id.org/sparql-generate/ite/JSON_Path_jayway>}.
+ * It takes two parameters as input:
+ * <ul>
+ * <li>a RDF Literal with datatype URI
+ * {@code <urn:iana:mime:application/json>}</li>
+ * <li>a RDF Literal with datatype {@code xsd:string}</li>
+ * </ul>
+ * and returns a list of RDF Literal with datatype URI
+ * {@code <urn:iana:mime:application/json>}.
+ *
+ * @author maxime.lefrancois
+ */
+public class ITE_CBOR extends IteratorFunctionBase2 {
+
+    static {
+        Configuration.setDefaults(new Configuration.Defaults() {
+
+            private final JsonProvider jsonProvider = new JacksonJsonProvider();
+            private final MappingProvider mappingProvider
+                    = new JacksonMappingProvider();
+
+            @Override
+            public JsonProvider jsonProvider() {
+                return jsonProvider;
+            }
+
+            @Override
+            public MappingProvider mappingProvider() {
+                return mappingProvider;
+            }
+
+            @Override
+            public Set<Option> options() {
+                return EnumSet.noneOf(Option.class);
+            }
+        });
+    }
+
+    /**
+     * The logger.
+     */
+    private static final Logger LOG = Logger.getLogger(ITE_CBOR.class);
+
+    /**
+     * The SPARQL function URI.
+     */
+    public static final String URI = SPARQLGenerate.ITE + "CBOR";
+
+    /**
+     * The datatype URI of the first parameter and the return literals.
+     */
+    private static final String datatypeUri = "urn:iana:mime:application/json";
+
+    /**
+     * {@inheritDoc }
+     */
+    @Override
+    public List<NodeValue> exec(NodeValue cbor, NodeValue jsonquery) {
+        /*
+        if (json.getDatatypeURI() == null
+                && datatypeUri == null
+                || json.getDatatypeURI() != null
+                && !json.getDatatypeURI().equals(datatypeUri)
+                && !json.getDatatypeURI().equals("http://www.w3.org/2001/XMLSchema#string")) {
+            LOG.warn("The URI of NodeValue1 MUST be"
+                    + " <" + datatypeUri + "> or"
+                    + " <http://www.w3.org/2001/XMLSchema#string>. Got <"
+                    + json.getDatatypeURI() + ">. Returning null.");
+        }
+        */
+        
+        Configuration conf = Configuration.builder()
+                .options(Option.ALWAYS_RETURN_LIST).build();
+
+        String json = Base64.base64Decode(cbor.asNode().getLiteralLexicalForm());
+        
+        try {
+            List<Object> values = JsonPath
+                    .using(conf)
+                    .parse(json)
+                    .read(jsonquery.getString());
+            List<NodeValue> nodeValues = new ArrayList<>(values.size());
+            Gson gson = new Gson();
+            for (Object value : values) {
+                RDFDatatype dt = TypeMapper.getInstance().getSafeTypeByName(datatypeUri);
+                String jsonstring = gson.toJson(value);
+                Node node = NodeFactory.createLiteral(jsonstring, dt);
+                NodeValue nodeValue = new NodeValueNode(node);
+                nodeValues.add(nodeValue);
+            }
+            return nodeValues;
+        } catch (Exception e) {
+            throw new ExprEvalException("FunctionBase: no evaluation", e);
+        }
+    }
+}
